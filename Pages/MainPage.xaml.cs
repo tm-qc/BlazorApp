60c49dfd921f9873b992636903b346fd201d5a79
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2025-02-05 16:21:24 +0900

    xamlがまだ全然つかめてないけど、なんとなく追加してみた
    ・ボタン
    ・メソッド

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -13,5 +13,31 @@ namespace BlazorApp.Pages
             InitializeComponent();
             BindingContext = model;
         }
+
+        /// <summary>
+        /// 自分で試しに追加してみた関数
+        /// C:\data\BlazorApp\Pages\MainPage.xamlのButtonに紐づくメソッドっぽい
+        /// 
+        ///  InitializeComponent();でビュー情報が読み込まれ、
+        ///  x:Name="CounterBtn"の名前と一致し、
+        ///  Clicked="OnCounterClicked"でイベントが紐づく。
+        ///   
+        /// そしてこの関数の処理のカウントが動きボタンにTextが挿入され表示される
+        ///  
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OnCounterClicked(object sender, EventArgs e)
+        {
+            count += 10;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
     }
 }
